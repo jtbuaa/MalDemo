6111c47ff571a4d79208f894a67a522e698e22d3
jingtao <jingtao@smartisan.cn>
2016-02-18 17:36:07 +0800

    replace button with checkbox

--- app/src/main/java/mal/demo/Activity1.java
+++ app/src/main/java/mal/demo/Activity1.java
@@ -6,20 +6,22 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
+import android.content.SharedPreferences;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.CheckBox;
 
 public class Activity1 extends Activity {
     String TAG = "================";
     String name = "activity1 ";
     Context mContext;
+    CheckBox cbx1, cbx2, cbx3, cbx4, cbx5, cbx6;
+    private SharedPreferences mPrefs;
 
     private LocationManager mLocationManager;
     private AlarmManager mAlarmManager;
@@ -30,6 +32,8 @@ public class Activity1 extends Activity {
         mContext = this;
         setContentView(R.layout.main);
         setTitle(name);
+
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(mContext);
 
         mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
@@ -51,45 +55,93 @@ public class Activity1 extends Activity {
         OnClickListener listener = new OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.btn1:
-                        // should not set minTime and minDistance to 0 which will drain the battery. just for demo here.
-                        mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, pIntentBroadCast);
+                CheckBox cbx = (CheckBox)v;
+                SharedPreferences.Editor editor = mPrefs.edit();
+                switch (cbx.getId()) {
+                    case R.id.cbx1:
+                        editor.putBoolean("key1", cbx.isChecked());
+                        if (cbx.isChecked()) {
+                            // should not set minTime and minDistance to 0 which will drain the battery. just for demo here.
+                            mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, pIntentActivity);
+                        } else {
+                            mLocationManager.removeUpdates(pIntentActivity);
+                        }
                         break;
-                    case R.id.btn2:
-                        // should not set minTime and minDistance to 0 which will drain the battery. just for demo here.
-                        mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, pIntentActivity);
+                    case R.id.cbx2:
+                        editor.putBoolean("key2", cbx.isChecked());
+                        if (cbx.isChecked()) {
+                            // should not set minTime and minDistance to 0 which will drain the battery. just for demo here.
+                            mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, pIntentBroadCast);
+                        } else {
+                            mLocationManager.removeUpdates(pIntentBroadCast);
+                        }
                         break;
-                    case R.id.btn3:
-                        // should not set minTime and minDistance to 0 which will drain the battery. just for demo here.
-                        mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, pIntentService);
-                    case R.id.btn4:
-                        mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                                triggerAtTime, interval, pIntentBroadCast);
+                    case R.id.cbx3:
+                        editor.putBoolean("key3", cbx.isChecked());
+                        if (cbx.isChecked()) {
+                            // should not set minTime and minDistance to 0 which will drain the battery. just for demo here.
+                            mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, pIntentService);
+                        } else {
+                            mLocationManager.removeUpdates(pIntentService);
+                        }
+                    case R.id.cbx4:
+                        editor.putBoolean("key4", cbx.isChecked());
+                        if (cbx.isChecked()) {
+                            mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                                    triggerAtTime, interval, pIntentActivity);
+                        } else {
+                            mAlarmManager.cancel(pIntentActivity);
+                        }
                         break;
-                    case R.id.btn5:
-                        mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                                triggerAtTime, interval, pIntentActivity);
+                    case R.id.cbx5:
+                        editor.putBoolean("key5", cbx.isChecked());
+                        if (cbx.isChecked()) {
+                            mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                                    triggerAtTime, interval, pIntentBroadCast);
+                        } else {
+                            mAlarmManager.cancel(pIntentBroadCast);
+                        }
                         break;
-                    case R.id.btn6:
-                        mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                                triggerAtTime, interval, pIntentService);
+                    case R.id.cbx6:
+                        editor.putBoolean("key6", cbx.isChecked());
+                        if (cbx.isChecked()) {
+                            mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                                    triggerAtTime, interval, pIntentService);
+                        } else {
+                            mAlarmManager.cancel(pIntentService);
+                        }
                         break;
                 }
+                editor.commit();
             }
         };
-        Button btn1 = (Button) findViewById(R.id.btn1);
-        btn1.setOnClickListener(listener);
-        Button btn2 = (Button) findViewById(R.id.btn2);
-        btn2.setOnClickListener(listener);
-        Button btn3 = (Button) findViewById(R.id.btn3);
-        btn3.setOnClickListener(listener);
-        Button btn4 = (Button) findViewById(R.id.btn4);
-        btn4.setOnClickListener(listener);
-        Button btn5 = (Button) findViewById(R.id.btn5);
-        btn5.setOnClickListener(listener);
-        Button btn6 = (Button) findViewById(R.id.btn6);
-        btn6.setOnClickListener(listener);
+        cbx1 = (CheckBox) findViewById(R.id.cbx1);
+        cbx1.setOnClickListener(listener);
+        cbx2 = (CheckBox) findViewById(R.id.cbx2);
+        cbx2.setOnClickListener(listener);
+        cbx3 = (CheckBox) findViewById(R.id.cbx3);
+        cbx3.setOnClickListener(listener);
+        cbx4 = (CheckBox) findViewById(R.id.cbx4);
+        cbx4.setOnClickListener(listener);
+        cbx5 = (CheckBox) findViewById(R.id.cbx5);
+        cbx5.setOnClickListener(listener);
+        cbx6 = (CheckBox) findViewById(R.id.cbx6);
+        cbx6.setOnClickListener(listener);
+        if (savedInstanceState != null) {
+            cbx1.setChecked(savedInstanceState.getBoolean("key1", false));
+            cbx2.setChecked(savedInstanceState.getBoolean("key2", false));
+            cbx3.setChecked(savedInstanceState.getBoolean("key3", false));
+            cbx4.setChecked(savedInstanceState.getBoolean("key4", false));
+            cbx5.setChecked(savedInstanceState.getBoolean("key5", false));
+            cbx6.setChecked(savedInstanceState.getBoolean("key6", false));
+        } else {
+            cbx1.setChecked(mPrefs.getBoolean("key1", false));
+            cbx2.setChecked(mPrefs.getBoolean("key2", false));
+            cbx3.setChecked(mPrefs.getBoolean("key3", false));
+            cbx4.setChecked(mPrefs.getBoolean("key4", false));
+            cbx5.setChecked(mPrefs.getBoolean("key5", false));
+            cbx6.setChecked(mPrefs.getBoolean("key6", false));
+        }
     }
 
     @Override
@@ -131,7 +183,24 @@ public class Activity1 extends Activity {
     @Override
     protected void onNewIntent(Intent intent) {
         Log.d(TAG, name + "onNewIntent() executed");
-
         super.onNewIntent(intent);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, name + "onSaveInstanceState() executed");
+        outState.putBoolean("key1", cbx1.isChecked());
+        outState.putBoolean("key2", cbx2.isChecked());
+        outState.putBoolean("key3", cbx3.isChecked());
+        outState.putBoolean("key4", cbx4.isChecked());
+        outState.putBoolean("key5", cbx5.isChecked());
+        outState.putBoolean("key6", cbx6.isChecked());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle inState) {
+        Log.d(TAG, name + "onRestoreInstanceState() executed");
+        super.onRestoreInstanceState(inState);
     }
 }
