87b7a2cd98731b52e92591d8ce744fb7eb673e1b
jingtao <jingtao@jingtao-HP-ENVY-4-Notebook-PC.(none)>
2016-03-12 10:03:45 +0800

    add test fake sms

--- app/src/main/java/mal/demo/Activity1.java
+++ app/src/main/java/mal/demo/Activity1.java
@@ -10,20 +10,24 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.DataSetObserver;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.preference.PreferenceManager;
+import android.telephony.PhoneNumberUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class Activity1 extends Activity implements OnAccountsUpdateListener {
@@ -31,14 +35,27 @@ public class Activity1 extends Activity implements OnAccountsUpdateListener {
     private String name = "activity1 ";
     private Context mContext;
     private ListView mMalList;
+    private static final String A_BY_LOCATION = "activity by location";
+    private static final String B_BY_LOCATION = "broadcast by location";
+    private static final String S_BY_LOCATION = "service by location";
+    private static final String A_BY_ALARM = "activity by alarm";
+    private static final String B_BY_ALARM = "broadcast by alarm";
+    private static final String S_BY_ALARM = "service by alarm";
+    private static final String BY_ACCOUNT = "by account";
+    public static final String FAKE_SMS = "test fake sms";
+    private static final String BY_TELEPHONY = "by telephony?";
+    private static final String BY_JOB_SCHEDULER = "by job Scheduler";
     private static final String[] MALS = new String[] {
-        "activity by location",
-        "broadcast by location",
-        "service by location",
-        "activity by alarm",
-        "broadcast by alarm",
-        "service by alarm",
-        "by account",
+            A_BY_LOCATION,
+            B_BY_LOCATION,
+            S_BY_LOCATION,
+            A_BY_ALARM,
+            B_BY_ALARM,
+            S_BY_ALARM,
+            BY_ACCOUNT,
+            FAKE_SMS,
+            BY_TELEPHONY,
+            BY_JOB_SCHEDULER,
     };
     private List<MalMethod> mMethods = new ArrayList<MalMethod>();
     private MalAdapter mAdapter;
@@ -98,65 +115,120 @@ public class Activity1 extends Activity implements OnAccountsUpdateListener {
                 SharedPreferences.Editor editor = mPrefs.edit();
                 editor.putBoolean(mAdapter.getItem(index).name, isChecked);
                 editor.commit();
-                switch (index) {
-                    case 0:
-                        if (isChecked) {
-                            // should not set minTime and minDistance to 0 which will drain the battery. just for demo here.
-                            mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, pIntentActivity);
-                        } else {
-                            mLocationManager.removeUpdates(pIntentActivity);
-                        }
-                        break;
-                    case 1:
-                        if (isChecked) {
-                            // should not set minTime and minDistance to 0 which will drain the battery. just for demo here.
-                            mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, pIntentBroadCast);
-                        } else {
-                            mLocationManager.removeUpdates(pIntentBroadCast);
-                        }
-                        break;
-                    case 2:
-                        if (isChecked) {
-                            // should not set minTime and minDistance to 0 which will drain the battery. just for demo here.
-                            mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, pIntentService);
-                        } else {
-                            mLocationManager.removeUpdates(pIntentService);
-                        }
-                        break;
-                    case 3:
-                        if (isChecked) {
-                            mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                                    triggerAtTime, interval, pIntentActivity);
-                        } else {
-                            mAlarmManager.cancel(pIntentActivity);
-                        }
-                        break;
-                    case 4:
-                        if (isChecked) {
-                            mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                                    triggerAtTime, interval, pIntentBroadCast);
-                        } else {
-                            mAlarmManager.cancel(pIntentBroadCast);
-                        }
-                        break;
-                    case 5:
-                        if (isChecked) {
-                            mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                                    triggerAtTime, interval, pIntentBroadCast);
-                        } else {
-                            mAlarmManager.cancel(pIntentBroadCast);
-                        }
-                        break;
-                    case 6:
-                        if (isChecked) {
-                            AccountManager.get(mContext).addOnAccountsUpdatedListener(Activity1.this, null, false);
-                        } else {
-                            AccountManager.get(mContext).removeOnAccountsUpdatedListener(Activity1.this);
-                        }
-                        break;
+                if (A_BY_LOCATION.equals(MALS[index])) {
+                    if (isChecked) {
+                        // should not set minTime and minDistance to 0 which will drain the battery. just for demo here.
+                        mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, pIntentActivity);
+                    } else {
+                        mLocationManager.removeUpdates(pIntentActivity);
+                    }
+                } else if (B_BY_LOCATION.equals(MALS[index])) {
+                    if (isChecked) {
+                        // should not set minTime and minDistance to 0 which will drain the battery. just for demo here.
+                        mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, pIntentBroadCast);
+                    } else {
+                        mLocationManager.removeUpdates(pIntentBroadCast);
+                    }
+                } else if (S_BY_LOCATION.equals(MALS[index])) {
+                    if (isChecked) {
+                        // should not set minTime and minDistance to 0 which will drain the battery. just for demo here.
+                        mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, pIntentService);
+                    } else {
+                        mLocationManager.removeUpdates(pIntentService);
+                    }
+                } else if (A_BY_ALARM.equals(MALS[index])) {
+                    if (isChecked) {
+                        mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                                triggerAtTime, interval, pIntentActivity);
+                    } else {
+                        mAlarmManager.cancel(pIntentActivity);
+                    }
+                } else if (B_BY_ALARM.equals(MALS[index])) {
+                    if (isChecked) {
+                        mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                                triggerAtTime, interval, pIntentBroadCast);
+                    } else {
+                        mAlarmManager.cancel(pIntentBroadCast);
+                    }
+                } else if (S_BY_ALARM.equals(MALS[index])) {
+                    if (isChecked) {
+                        mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                                triggerAtTime, interval, pIntentBroadCast);
+                    } else {
+                        mAlarmManager.cancel(pIntentBroadCast);
+                    }
+                } else if (BY_ACCOUNT.equals(MALS[index])) {
+                    if (isChecked) {
+                        AccountManager.get(mContext).addOnAccountsUpdatedListener(Activity1.this, null, false);
+                    } else {
+                        AccountManager.get(mContext).removeOnAccountsUpdatedListener(Activity1.this);
+                    }
                 }
             }
         });
+    }
+
+    //http://stackoverflow.com/questions/12335642/create-pdu-for-android-that-works-with-smsmessage-createfrompdu-gsm-3gpp
+    public static void createFakeSms(Context context, String sender,
+                                      String body) {
+        byte[] pdu = null;
+        byte[] scBytes = PhoneNumberUtils
+                .networkPortionToCalledPartyBCD("0000000000");
+        byte[] senderBytes = PhoneNumberUtils
+                .networkPortionToCalledPartyBCD(sender);
+        int lsmcs = scBytes.length;
+        byte[] dateBytes = new byte[7];
+        Calendar calendar = new GregorianCalendar();
+        dateBytes[0] = reverseByte((byte) (calendar.get(Calendar.YEAR)));
+        dateBytes[1] = reverseByte((byte) (calendar.get(Calendar.MONTH) + 1));
+        dateBytes[2] = reverseByte((byte) (calendar.get(Calendar.DAY_OF_MONTH)));
+        dateBytes[3] = reverseByte((byte) (calendar.get(Calendar.HOUR_OF_DAY)));
+        dateBytes[4] = reverseByte((byte) (calendar.get(Calendar.MINUTE)));
+        dateBytes[5] = reverseByte((byte) (calendar.get(Calendar.SECOND)));
+        dateBytes[6] = reverseByte((byte) ((calendar.get(Calendar.ZONE_OFFSET) + calendar
+                .get(Calendar.DST_OFFSET)) / (60 * 1000 * 15)));
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            bo.write(lsmcs);
+            bo.write(scBytes);
+            bo.write(0x04);
+            bo.write((byte) sender.length());
+            bo.write(senderBytes);
+            bo.write(0x00);
+            bo.write(0x00); // encoding: 0 for default 7bit
+            bo.write(dateBytes);
+            try {
+                String sReflectedClassName = "com.android.internal.telephony.GsmAlphabet";
+                Class cReflectedNFCExtras = Class.forName(sReflectedClassName);
+                Method stringToGsm7BitPacked = cReflectedNFCExtras.getMethod(
+                        "stringToGsm7BitPacked", new Class[] { String.class });
+                stringToGsm7BitPacked.setAccessible(true);
+                byte[] bodybytes = (byte[]) stringToGsm7BitPacked.invoke(null,
+                        body);
+                bo.write(bodybytes);
+            } catch (Exception e) {
+            }
+
+            pdu = bo.toByteArray();
+        } catch (IOException e) {
+        }
+
+        Intent intent = new Intent();
+        intent.setClassName("com.android.mms",
+                "com.android.mms.transaction.SmsReceiverService");
+        intent.setAction("android.provider.Telephony.SMS_RECEIVED");
+        intent.putExtra("pdus", new Object[] { pdu });
+        intent.putExtra("format", "3gpp");
+        try {
+            context.startService(intent);
+        } catch(SecurityException e) {
+            Toast.makeText(context, e.toString(), Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    private static byte reverseByte(byte b) {
+        return (byte) ((b & 0xF0) >> 4 | (b & 0x0F) << 4);
     }
 
     public void onAccountsUpdated(Account[] a) {
--- app/src/main/java/mal/demo/MalAdapter.java
+++ app/src/main/java/mal/demo/MalAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckedTextView;
 
 import java.util.List;
@@ -15,22 +16,39 @@ import java.util.List;
 public class MalAdapter extends ArrayAdapter<MalMethod> {
     private int mResourceId;
     private LayoutInflater mInflater;
+    private Context mContext;
 
     public MalAdapter(Context context, int resId, List<MalMethod> methods) {
         super(context, resId, methods);
         mResourceId = resId;
         mInflater = LayoutInflater.from(context);
+        mContext = context;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         MalMethod malMethod = getItem(position);
+        boolean isButton = Activity1.FAKE_SMS.equals(malMethod.name);
         if (convertView == null) {
-            convertView = mInflater.inflate(mResourceId, null);
+            if (isButton)
+                convertView = mInflater.inflate(R.layout.list_button, null);
+            else
+                convertView = mInflater.inflate(mResourceId, null);
         }
-        CheckedTextView methodView = (CheckedTextView) convertView.findViewById(R.id.list_row_ctv);
-        methodView.setText(malMethod.name);
-        methodView.setChecked(malMethod.checked);
+        if (isButton) {
+            Button button = (Button) convertView.findViewById(R.id.list_row_button);
+            button.setText(malMethod.name);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Activity1.createFakeSms(mContext, "10086", "wow, you are out of data quota");
+                }
+            });
+        } else {
+            CheckedTextView methodView = (CheckedTextView) convertView.findViewById(R.id.list_row_ctv);
+            methodView.setText(malMethod.name);
+            methodView.setChecked(malMethod.checked);
+        }
 
         return convertView;
     }
